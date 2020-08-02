bea5d3f66b461b94b5646ab35f67bfafbe6e7c08
Guilouf <Guilouf@users.noreply.github.com>
2020-08-02 20:02:28 +0200

    Tag system for both Company and Receipt

--- project_receipt/receipt/models.py
+++ project_receipt/receipt/models.py
@@ -3,7 +3,13 @@ from django.urls import reverse
 
 
 class Tag(models.Model):
+    class TagCategory(models.IntegerChoices):
+        RECEIPT = 0, 'receipt'
+        COMPANY = 1, 'company'
+        BOTH = 2, 'both'
+
     name = models.CharField(max_length=200)
+    category = models.PositiveSmallIntegerField(choices=TagCategory.choices)
 
     def __str__(self):
         return self.name
@@ -14,6 +20,9 @@ class Tag(models.Model):
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag, blank=True,
+                                  limit_choices_to={'category__in': [Tag.TagCategory.COMPANY, Tag.TagCategory.BOTH]}
+                                  )
 
     def __str__(self):
         return self.name
@@ -37,7 +46,9 @@ class Receipt(models.Model):
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateTimeField()
     establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True,
+                                  limit_choices_to={'category__in': [Tag.TagCategory.RECEIPT, Tag.TagCategory.BOTH]}
+                                  )
 
     def __str__(self):
         return f'{self.amount}-{self.establishment}-{self.date}'
