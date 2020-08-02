cfd55f1327b51dff08bea8d1712b7a15b4bf2737
Guilouf <Guilouf@users.noreply.github.com>
2020-08-02 14:05:24 +0200

    rename shop to establishment

--- project_receipt/receipt/models.py
+++ project_receipt/receipt/models.py
@@ -26,8 +26,8 @@ class Establishment(models.Model):
 class Receipt(models.Model):
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateTimeField()
-    shop = models.ForeignKey(Establishment, on_delete=models.CASCADE)
+    establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
-        return f'{self.amount}-{self.shop}-{self.date}'
+        return f'{self.amount}-{self.establishment}-{self.date}'
