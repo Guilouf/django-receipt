b0a979fd814370ef0db0ab4e30d33dd770cc4e80
Guilouf <Guilouf@users.noreply.github.com>
2020-08-22 22:21:45 +0200

    Index url

--- project_receipt/receipt/urls.py
+++ project_receipt/receipt/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from receipt import views
 
 urlpatterns = [
+    path('', views.ReceiptList.as_view(), name='home'),
     path('receipt/', views.ReceiptList.as_view(), name='receipt_list'),
     path('receipt/create', views.ReceiptCreate.as_view(), name='receipt_create'),
     path('receipt/<int:pk>/edit', views.ReceiptUpdate.as_view(), name='receipt_update'),
