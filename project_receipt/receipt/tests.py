4fd1aae43a9f4968fe91bd510cf4c414dffb78cd
Guilouf <Guilouf@users.noreply.github.com>
2021-04-05 22:29:36 +0200

    Stupid first test with fixtures

--- project_receipt/receipt/tests.py
+++ project_receipt/receipt/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from receipt.models import Receipt
+
+
+class AmountsTestCase(TestCase):
+    fixtures = ['receipt_test_fixture']
+
+    def test_total_receipt_amount(self):
+        """Check total amount of all existing tickets"""
+        self.assertEqual(Receipt.objects.all().total(), '55.70')
