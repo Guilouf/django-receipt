0a498110cca21a4e2f148707dd4b6b24320d3137
Guilouf <Guilouf@users.noreply.github.com>
2021-09-25 15:12:05 +0200

    Use RangeFilter for amount

--- project_receipt/receipt/filters.py
+++ project_receipt/receipt/filters.py
@@ -1,4 +1,4 @@
-from django_filters import FilterSet, DateFromToRangeFilter
+from django_filters import FilterSet, DateFromToRangeFilter, RangeFilter
 from django_filters.widgets import RangeWidget
 
 from receipt.models import Receipt, Establishment, Company
@@ -7,11 +7,11 @@ from receipt.models import Receipt, Establishment, Company
 class ReceiptFilter(FilterSet):
     # im not filtering on datetime, if needed i can use isodatetimefilter
     date_range = DateFromToRangeFilter(field_name='date', widget=RangeWidget(attrs={'type': 'date'}))
+    amount_range = RangeFilter(field_name='amount')
 
     class Meta:
         model = Receipt
-        fields = {'amount': ['gte', 'lte'],
-                  'tags': ['exact'],
+        fields = {'tags': ['exact'],
                   'establishment__name': ['icontains'],
                   'establishment__company__name': ['icontains']}
 
